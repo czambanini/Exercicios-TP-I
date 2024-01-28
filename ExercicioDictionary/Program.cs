bf8cd76382c9716b87750c1e06fc99681bef8dea
czambanini <ca.zambanini@gmail.com>
2024-01-28 11:17:02 -0300

    resolução do exercicio de dictionary

--- ExercicioDictionary/Program.cs
+++ ExercicioDictionary/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Escreva uma frase: ");
+            string frase = Console.ReadLine();
+            ContadorDePalavras(frase);
+
+
+
+            void ContadorDePalavras(string frase)
+            {
+                frase = frase.Replace(",", "").Replace(".", "").ToUpper();
+                string[] palavras = frase.Split(' ');
+                string[] palavrasSemRepeticao = palavras.Distinct().ToArray();
+
+                Dictionary<string, int> contadorDePalavras = new Dictionary<string, int>();
+
+                foreach (string palavra in palavrasSemRepeticao)
+                {
+                    contadorDePalavras.Add(palavra, 0);
+                }
+
+                foreach (string palavra in palavras)
+                {
+                    contadorDePalavras[palavra]++;
+                }
+
+                Console.WriteLine(" ");
+                foreach (KeyValuePair<string, int> kvp in contadorDePalavras)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value} vezes.");
+                }
+            }
+            
+
         }
     }
 }
