ca1eaca428e86eb72d638e849009aeb1c0ae1784
diegorafael <diegorafael_si@msn.com>
2024-01-28 21:00:32 +0000

    Inclusão de comentários e soluções alternativas.

--- ExercicioDictionary/Program.cs
+++ ExercicioDictionary/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Escreva uma frase: ");
             string frase = Console.ReadLine();
             ContadorDePalavras(frase);
+            ContadorDePalavras2(frase);
 
 
 
@@ -35,7 +36,30 @@
                     Console.WriteLine($"{kvp.Key} -> {kvp.Value} vezes.");
                 }
             }
-            
+
+            void ContadorDePalavras2(string frase)
+            {
+                Console.WriteLine("IMPLEMENTAÇÃO ALTERNATIVA");
+
+                frase = frase.Replace(",", "").Replace(".", "").ToUpper();
+                string[] palavras = frase.Split(' ');
+
+                Dictionary<string, int> contadorDePalavras = new Dictionary<string, int>();
+
+                foreach (string palavra in palavras)
+                {
+                    if(contadorDePalavras.ContainsKey(palavra))
+                        contadorDePalavras[palavra]++;
+                    else
+                        contadorDePalavras.Add(palavra,1);
+                }
+
+                Console.WriteLine(" ");
+                foreach (KeyValuePair<string, int> kvp in contadorDePalavras)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value} vezes.");
+                }
+            }
 
         }
     }
